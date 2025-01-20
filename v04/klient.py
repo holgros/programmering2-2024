64874c003ea18750c16afad80d07f16a720625fa
Holger Rosencrantz <holgros@gmail.com>
2025-01-20 14:26:55 +0100

    Socketexempel med enkel meddelandehantering mellan klient och server

--- v04/klient.py
+++ v04/klient.py
@@ -8,3 +8,12 @@ def connect_to_server():
     s.connect((host, port))     # Anslut till servern
     return s
 s = connect_to_server()
+# Ta emot ett meddelande från servern
+b = s.recv(1024)
+msg = b.decode("utf-16")    # Gör om meddelandet från bytekod till vanlig text
+print(msg)
+# Skicka ett eget meddelande till servern
+msg = input("Skriv något till servern:")
+b = msg.encode("utf-16")
+s.send(b)
+s.close()
--- v04/server.py
+++ v04/server.py
@@ -1,13 +1,25 @@
 from socket import *
 def start_server():
     s = socket()            # Skapa ett socket-objekt
-    host = "localhost"      # som körs på den egna datorn
+    host = "10.32.42.137"      # som körs på den egna datorn
     port = 12345            # på port 12345.
     s.bind((host, port))    # Konfigurera socket-objektet.
     s.listen()              # Vänta på att klient ansluter.
     return s
 s = start_server()
-print("Servern är igång på port 12345! Väntar på att en klient ska ansluta...")
-conn, addr = s.accept()     # När en klient ansluter
-print("En klient anslöt från adressen", addr)
+while True:
+    print("Servern är igång på port 12345! Väntar på att en klient ska ansluta...")
+    conn, addr = s.accept()     # När en klient ansluter
+    print("En klient anslöt från adressen", addr)
+    # Skicka meddelande från servern
+    msg = "Servern säger hej till klienten! Hur mår du idag?"
+    b = msg.encode("utf-16")    # Gör om meddelandet till bytekod
+    conn.send(b)                # Skicka meddelandet till klienten
+    print("Väntar på meddelande från klienten...")
+    # Ta emot ett meddelande från klienten
+    b = conn.recv(1024)
+    msg = b.decode("utf-16")    # Gör om från bytekod till vanlig text
+    print(msg)
+    # Stäng anslutningen till klienten
+    conn.close()
 conn.close()
